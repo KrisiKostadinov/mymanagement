412f3f2a92a2366c7f22a5c390230b600166c306
KrisiKostadinov <krisi.199898@gmail.com>
2020-08-04 15:18:32 +0300

    Added adding of admin user if db is empty!

--- config/db.js
+++ config/db.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 const uri = process.env.URI;
 
@@ -8,4 +10,16 @@ mongoose.connect(uri, {
     useNewUrlParser: true,
 }, () => {
     console.log('Connected to db');
+
+    const users = await User.find();
+
+    if(users.length === 0) {
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash('123456', salt);
+
+        User.create({
+            email: 'admin@admin.com',
+            passwordHash: hash
+        });
+    }
 });
